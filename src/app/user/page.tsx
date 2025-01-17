d2b14aad8cb907042d4b9a0bf44e7673186163ce
Andres Chavez <andreschavezcdm@gmail.com>
2025-01-16 23:41:20 -0300

    manejo-pantalla

---
+++ src/app/user/page.tsx
@@ -0,0 +1,11 @@
+/* aqui traemos el componente de UserProfile */
+
+import UserProfile from "@/components/User/UserProfile";
+
+
+const ProfilePage = () => {
+    return <UserProfile />;
+};
+
+
+export default ProfilePage;
